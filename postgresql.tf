b4f11dbeaf169a3bf3b421f53da18ffa53f53b53
fxshell <feeedamatta@gmail.com>
2026-06-14 22:21:49 -0300

    [Feature] refatora PostgreSQL para ngo, donation e volunteer service

--- postgresql.tf
+++ postgresql.tf
@@ -1,54 +1,6 @@
-
-resource "azurerm_postgresql_flexible_server" "auth" {
-  name                   = "pg-auth-solidarytech-fase5"
-  location               = "centralus"
-  resource_group_name    = azurerm_resource_group.rg.name
-  version                = "15"
-  administrator_login    = "adminuser"
-  administrator_password = var.db_admin_password
-  zone                   = "2"
-
-  storage_mb                   = 32768
-  backup_retention_days        = 7
-  geo_redundant_backup_enabled = false
-
-  sku_name = "B_Standard_B1ms"
-
-  # Habilitar acesso público
-  public_network_access_enabled = true
-
-  tags = local.common_tags
-
-  depends_on = [azurerm_private_dns_zone_virtual_network_link.postgresql_vnet_link]
-}
-
-
-resource "azurerm_postgresql_flexible_server" "flag" {
-  name                   = "pg-flag-solidarytech-fase5"
-  location               = "centralus"
-  resource_group_name    = azurerm_resource_group.rg.name
-  version                = "15"
-  administrator_login    = "adminuser"
-  administrator_password = var.db_admin_password
-
-  storage_mb                   = 32768
-  backup_retention_days        = 7
-  geo_redundant_backup_enabled = false
-  zone                         = "2"
-
-  sku_name = "B_Standard_B1ms"
-
-  # Desabilitar acesso público
-  public_network_access_enabled = false
-
-  tags = local.common_tags
-
-  depends_on = [azurerm_private_dns_zone_virtual_network_link.postgresql_vnet_link]
-}
-
-resource "azurerm_postgresql_flexible_server" "targeting" {
-  name                   = "pg-targeting-solidarytech-fase5"
-  location               = "centralus"
+resource "azurerm_postgresql_flexible_server" "ngo" {
+  name                   = "pg-ngo-solidarytech-f5"
+  location               = var.aks_location
   resource_group_name    = azurerm_resource_group.rg.name
   version                = "15"
   administrator_login    = "adminuser"
@@ -61,11 +13,53 @@ resource "azurerm_postgresql_flexible_server" "targeting" {
 
   sku_name = "B_Standard_B1ms"
 
-  # Desabilitar acesso público
   public_network_access_enabled = false
 
-  tags = local.common_tags
+  tags = merge(local.common_tags, { Service = "ngo-service" })
 
   depends_on = [azurerm_private_dns_zone_virtual_network_link.postgresql_vnet_link]
 }
 
+resource "azurerm_postgresql_flexible_server" "donation" {
+  name                   = "pg-donation-solidarytech-f5"
+  location               = var.aks_location
+  resource_group_name    = azurerm_resource_group.rg.name
+  version                = "15"
+  administrator_login    = "adminuser"
+  administrator_password = var.db_admin_password
+  zone                   = "2"
+
+  storage_mb                   = 32768
+  backup_retention_days        = 7
+  geo_redundant_backup_enabled = true
+
+  sku_name = "B_Standard_B1ms"
+
+  public_network_access_enabled = false
+
+  tags = merge(local.common_tags, { Service = "donation-service" })
+
+  depends_on = [azurerm_private_dns_zone_virtual_network_link.postgresql_vnet_link]
+}
+
+resource "azurerm_postgresql_flexible_server" "volunteer" {
+  name                   = "pg-volunteer-solidarytech-f5"
+  location               = var.aks_location
+  resource_group_name    = azurerm_resource_group.rg.name
+  version                = "15"
+  administrator_login    = "adminuser"
+  administrator_password = var.db_admin_password
+  zone                   = "1"
+
+  storage_mb                   = 32768
+  backup_retention_days        = 7
+  geo_redundant_backup_enabled = false
+
+  sku_name = "B_Standard_B1ms"
+
+  public_network_access_enabled = false
+
+  tags = merge(local.common_tags, { Service = "volunteer-service" })
+
+  depends_on = [azurerm_private_dns_zone_virtual_network_link.postgresql_vnet_link]
+}
